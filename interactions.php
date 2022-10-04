cf425964bdaa360d921c61136403e81e7570edc8
arfunkle <q.t.morrissey@gmail.com>
2022-10-04 15:20:20 +0200

    playing around with php

--- interactions.php
+++ interactions.php
@@ -3,9 +3,6 @@
 
 
 $stringData = json_encode($_POST["json"]);
-if (file_put_contents("words.json", $stringData))
-    echo "JSON file created successfully...";
-else 
-    echo "Oops! Error creating json file...";
+file_put_contents("words.json", $stringData)
 
 ?>
