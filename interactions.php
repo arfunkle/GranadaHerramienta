e584e3cfc9e7e9037238c2812e23affd91987578
arfunkle <q.t.morrissey@gmail.com>
2022-10-04 15:38:50 +0200

    revamping php

--- interactions.php
+++ interactions.php
@@ -1,8 +1,20 @@
 <?php
 
 
-
-$stringData = json_encode($_POST["json"]);
-file_put_contents("./words.json", $stringData)
-
+if($_POST['json'])
+{
+    $stringData = json_encode($_POST["json"]);
+    $open_json = fopen('words.json', 'w');
+    if($open_json){
+        if(fwrite($open_json, $_POST['json'] === FALSE))
+        {
+            die('failed to write to file');
+        }
+        fclose($open_json);
+    }
+    else
+    {
+        die('failed to open file');
+    }
+} else die('something went wrong!');
 ?>
